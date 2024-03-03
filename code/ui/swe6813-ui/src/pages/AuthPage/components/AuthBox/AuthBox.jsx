4461e3532a3045c92fc8d23a72c18c2a9b69553a
Matthew Maravilla <matthewmaravilla@Matthews-Laptop.local>
2024-03-03 08:00:27 -0500

    updated code with the react-mu library

---
+++ code/ui/swe6813-ui/src/pages/AuthPage/components/AuthBox/AuthBox.jsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+import Button from '../../../components/Button/Button';
+import '../../../../index.css';
+
+function AuthBox({boxType}) {
+        return ;
+};
+export default AuthBox;
