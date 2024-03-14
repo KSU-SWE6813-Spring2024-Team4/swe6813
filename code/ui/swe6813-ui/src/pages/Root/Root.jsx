a42e523d9a7a31e787b95a61b681a9c772e4e25b
Elliott Allmann <elliott.allmann@cox.net>
2024-03-13 21:26:37 -0700

    Test UI change

--- code/ui/swe6813-ui/src/pages/Root/Root.jsx
+++ code/ui/swe6813-ui/src/pages/Root/Root.jsx
@@ -37,6 +37,7 @@ export default function Root() {
     return(
         <div>
             <h1>Hi, Team 4 Project</h1>
+            <h2>Our CI/CD Workflow works correctly!</h2>
             {/* TODO: just here for testing full auth flow */}
             <Button
                 title="Sign Out"
