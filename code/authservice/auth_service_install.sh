47d3ae4e8472122407515c4d7ce208411cdca533
Elliott Allmann <elliott.allmann@cox.net>
2024-03-13 18:59:36 -0700

    Hard code stuff, might work?

--- code/authservice/auth_service_install.sh
+++ code/authservice/auth_service_install.sh
@@ -6,5 +6,12 @@ whoami
 
 ./mvnw package -DskipTests
 
-echo "Moving war file to target directory"
+echo "Moving war file to deployment directory directory"
+mv target/authservice-0.0.1-SNAPSHOT.war /var/www/auth_service/
+
+echo "Starting WAR file"
+java -jar /var/www/auth_service/authservice-0.0.1-SNAPSHOT.war &
+
+echo "Restarting apache"
+sudo systemctl restart apache2
 
