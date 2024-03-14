1e670d88a2c8ad3483a12cb1aa437d386a8243ee
Elliott Allmann <elliott.allmann@cox.net>
2024-03-13 19:26:38 -0700

    Added python config steps

--- services/main_service/api/main_service_install.sh
+++ services/main_service/api/main_service_install.sh
@@ -1,3 +1,15 @@
 #!/usr/bin/env bash
 
-echo "Main service install"
+echo "Configuring directory for install"
+python3 -m venv source/venv
+
+source source/venv/bin/activate
+
+pip install -r requirements.txt
+cp /var/www/.env source/.env
+
+echo "Copying files to install directory"
+cp -R . /var/www/main_service/api/
+
+echo "Restarting apache2"
+sudo systemctl restart apache2
