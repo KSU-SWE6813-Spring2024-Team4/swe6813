8cebbdf890c87aea6f9692aa83d1aa3729e53eca
Elliott Allmann <elliott.allmann@cox.net>
2024-03-29 11:56:31 -0700

    Add user following

--- services/main_service/api/source/app.py
+++ services/main_service/api/source/app.py
@@ -16,13 +16,13 @@ def create_app(test_config=None):
         graph = graph_db.GraphDb()
         graph.get_database_driver()
 
-    from .blueprints import games, user, user_game, friend_requests, friends
+    from .blueprints import games, user, user_game, friend_requests, friends, user_follow
     app.register_blueprint(games.bp)
     app.register_blueprint(user.bp)
     app.register_blueprint(user_game.bp)
     app.register_blueprint(friend_requests.bp)
     app.register_blueprint(friends.bp)
-
+    app.register_blueprint(user_follow.bp)
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
